fd30c3d44c7acf92e7e5410ab51b37fca731827c
kim-jun-young <jykim@gemiso.com>
2023-03-23 15:01:33 +0900

    next js config editted

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
-
+const repo = "juny-blog";
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
+  assetPrefix: `/${repo}/`,
+  basePath: `/${repo}`,
   images: {
     domains: [
       'www.notion.so',
